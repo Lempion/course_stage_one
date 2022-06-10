6b263dd274c2547752c7872560cff0ce5581c040
Vitaly <vit.fedor2012@mail.ru>
2022-06-10 13:13:42 +0300

    Реализована возможность регистрации пользователя

--- actions/registration.php
+++ actions/registration.php
@@ -1,3 +1,37 @@
 <?php
 session_start();
 
+require '../classes/DataBase.php';
+
+if (!$_POST) {
+    die();
+}
+
+$dataBase = new DataBase();
+
+$result = $dataBase->registrationUser($_POST['email'], $_POST['password']);
+
+/**
+ * Записываем в $_SESSION['ANSWER'] ответ от нашей функции
+ */
+$_SESSION['ANSWER'] = $result;
+
+/**
+ * 1. Делаем проверку на ключ ACCEPT, т.к вместе с ним при успешной регистрации придёт
+ * email, который нужно записать в $_SESSION['USER_EMAIL'].
+ *
+ * 2. Удаляем из сессии ANSWER ключ, что бы не дублировать
+ *
+ * 3. Делаем переход на страницу авторизации
+ */
+if ($result['ACCEPT']) {
+    $_SESSION['USER_EMAIL'] = $result['ACCEPT']['USER_EMAIL'];
+    unset($_SESSION['ANSWER']['USER_EMAIL']);
+
+    header('Location:../login.php');
+    exit();
+}
+
+header('Location:../index.php');
+exit();
+
--- classes/DataBase.php
+++ classes/DataBase.php
@@ -9,16 +9,33 @@ class DataBase
 
     public function __construct()
     {
-        $this->connectDB($this->hosts,$this->user,$this->pass);
+        $this->db = new PDO($this->hosts, $this->user, $this->pass);
     }
 
-    private function connectDB($hosts,$user,$pass){
-        $this->db = new PDO($hosts,$user,$pass);
+    public function registrationUser($email, $password): array
+    {
+        $sql = $this->db->prepare("SELECT * FROM `users` WHERE `email`=?");
+
+        $sql->execute(array($email));
+
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            return ['ERROR' => 'Этот эл. адрес уже занят другим пользователем.'];
+        }
+
+        $hashPass = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = ("INSERT INTO `users` (`email`,`password`) VALUES (?,?)");
+
+        $this->db->prepare($sql)->execute([$email, $hashPass]);
+
+        return ['ACCEPT' => 'Вы успешно зарегистрировались!', 'USER_EMAIL' => $email];
     }
 
-    public function registrationUser(){}
-
-    public function authorizationUser(){}
+    public function authorizationUser()
+    {
+    }
 
 
 }
